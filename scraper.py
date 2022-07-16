7b2d2e357748893659315e2355888e8ffb4d5df7
Myles Fung <mylesfung@gmail.com>
2022-07-16 02:53:00 -0700

    formatting

--- scraper.py
+++ scraper.py
@@ -3,14 +3,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
 
-
 driver = webdriver.Chrome(executable_path='/Users/myles/desktop/projects/tasting-the-soup/chromedriver')
 url = 'https://www.youtube.com/c/JordanPetersonVideos/videos?view=0&sort=p&flow=grid'
 driver.get(url)
 
 placards = driver.find_elements(By.CLASS_NAME, 'style-scope ytd-grid-video-renderer')
-
-videos = []
+all_videos = []
 
 for placard in placards:
     title = placard.find_element(By.XPATH, './/*[@id="video-title"]').text
@@ -18,15 +16,15 @@ for placard in placards:
     posted = placard.find_element(By.XPATH, './/*[@id="metadata-line"]/span[2]').text
     length = placard.find_element(By.XPATH, './/*[@id="text"]').text                           
 
-    video_info = {
+    video_data = {
         'Title': title,
         'Views': views,
         'Time Posted': posted,
         'Length': length
     }
-    videos.append(video_info)
+    all_videos.append(video_data)
 
-df = pd.DataFrame(videos)
+df = pd.DataFrame(all_videos)
 print(df)
 
 
