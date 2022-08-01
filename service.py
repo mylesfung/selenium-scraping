2d93091fcce5d937cc8fac923c0c1b96bf6ab61a
Myles Fung <mylesfung@gmail.com>
2022-07-31 20:16:59 -0700

    cleaning

--- service.py
+++ service.py
@@ -3,7 +3,7 @@ import gspread
 ## Authorizing Service Account bot access to Google Sheets. ##
 
 client = gspread.service_account()
-spreadsheet = client.open('JBP YouTube Data 7/16/22')
+spreadsheet = client.open('JBP YouTube Data as of 7/16/22 7:32PM PST')
 
 #for num in range(2, 32):
 #    print(spreadsheet.Newest.get('B' +  str(num)))
