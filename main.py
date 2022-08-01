6d0476dde8d5b72d3279e70c3a5b198e7c3d9587
Myles Fung <mylesfung@gmail.com>
2022-07-31 20:33:21 -0700

    comments

--- main.py
+++ main.py
@@ -7,15 +7,17 @@ import gspread
 from df2gspread import df2gspread as d2g
 from oauth2client.service_account import ServiceAccountCredentials
 
+
 ## Scraper ##
 
+# Start up ChromeDriver.
 driver = webdriver.Chrome(executable_path='/Users/myles/desktop/projects/selenium-scraper/chromedriver')
 
-# User-entered URL from YouTube channel's 'VIDEOS' page.
-# Videos can be sorted between popular/newest/oldest using YouTube's 'SORT BY' feature.
+# Enter URL from YouTube channel's 'VIDEOS' page.
 URL = 'https://www.youtube.com/c/JordanPetersonVideos/videos?view=0&sort=dd&flow=grid'
 driver.get(URL)
 
+# Locate video data and organize into dataframe.
 placards = driver.find_elements(By.CLASS_NAME, 'style-scope ytd-grid-video-renderer')
 all_videos = []
 
@@ -36,16 +38,20 @@ for placard in placards:
 df = pd.DataFrame(data=all_videos)
 #print df
 
+
 ## Database ##
 
+# Define APIs being accessed.
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
+# Service account authenticates to APIs via key file.
 credentials = ServiceAccountCredentials.from_json_keyfile_name('key.json', scope)
 client = gspread.authorize(credentials)
 
+# Identify target spreadsheet and worksheet.
+# Sort video category by popular/newest/oldest via YouTube's 'SORT BY' feature.
 spreadsheet_key = '1sz65hZuFaZomwZrqBmEUzanQtCVj_drUJYkmSNuEqmM'
-# User-entered target worksheet name.
 worksheet_name = 'Newest'
 d2g.upload(df, spreadsheet_key, worksheet_name, credentials=credentials, row_names=True)
 
