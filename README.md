9f79f5ac9da1e3cf649e121d13cbc435efa1a4f1
Myles Fung <mylesfung@gmail.com>
2022-08-14 04:51:34 -0700

    time intervals, error handling, key securing

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # selenium-scraping
 
-rsf-scrape : Python script adds UC Berkeley Recreational Sports Facility (RSF) weight room capacity data to [a Google Sheet](https://docs.google.com/spreadsheets/d/1x1JomLFPq3CShupUfJW60zzBmispI2-yzIbndF3skVk/edit#gid=0).
+rsf-scrape : Adds UC Berkeley Recreational Sports Facility (RSF) weight room capacity data to [a Google Sheet](https://docs.google.com/spreadsheets/d/1x1JomLFPq3CShupUfJW60zzBmispI2-yzIbndF3skVk/edit#gid=0).
 
-youtube-scrape : Python script uploads YouTube video data to [a Google Sheet](https://docs.google.com/spreadsheets/d/1sz65hZuFaZomwZrqBmEUzanQtCVj_drUJYkmSNuEqmM/edit#gid=1683412517).
+youtube-scrape : Uploads YouTube video data to [a Google Sheet](https://docs.google.com/spreadsheets/d/1sz65hZuFaZomwZrqBmEUzanQtCVj_drUJYkmSNuEqmM/edit#gid=1683412517).
--- rsf-scrape/main.py
+++ rsf-scrape/main.py
@@ -1,5 +1,3 @@
-# Mission: scrape RSF Meter and send capacity data to Google Sheet every 15 minutes.
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -18,15 +16,18 @@ from oauth2client.service_account import ServiceAccountCredentials
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 def add_row_data():
-    URL = 'https://safe.density.io/#/displays/dsp_956223069054042646?token=shr_o69HxjQ0BYrY2FPD9HxdirhJYcFDCeRolEd744Uj88e'
-    driver.get(URL)
-    time.sleep(5)
+    try:
+        URL = 'https://safe.density.io/#/displays/dsp_956223069054042646?token=shr_o69HxjQ0BYrY2FPD9HxdirhJYcFDCeRolEd744Uj88e'
+        driver.get(URL)
+        time.sleep(5)
 
-    date_time = time.ctime()[:4] + time.strftime('%m/%d/%Y %H:%M')
-    #date_time = time.ctime()[:16]
-    capacity = driver.find_element(By.XPATH, './/*[@id="root"]/div/div/div[1]/div[2]/div/span[1]').text[0]
-    
-    spreadsheet.append_row([date_time, capacity])
+        capacity = driver.find_element(By.XPATH, './/*[@id="root"]/div/div/div[1]/div[2]/div/span').text[0]
+        date_time = time.ctime()[:4] + time.strftime('%m/%d/%Y %H:%M')
+        #date_time = time.ctime()[:16]
+        
+        spreadsheet.append_row([date_time, capacity])
+    except Exception as e:
+        print(f'Density Display Load Error: {e}')
 
 
 #################
@@ -49,6 +50,10 @@ spreadsheet = client.open_by_key('1x1JomLFPq3CShupUfJW60zzBmispI2-yzIbndF3skVk')
 # Loop #
 ########
 
-for _ in range(3):
+# Due to the initial sleep period, each timestamp will be 
+# staggered from the previous by approximately 6 seconds.
+minute_intervals = 10
+
+while True:
     add_row_data()
-    time.sleep(5)
+    time.sleep((60 * minute_intervals))
